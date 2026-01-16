de82a2fccfa948b90a99e3598419d4edfcc3d0ef
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-01-16 13:42:17 -0500

    chore: make integration test app name constant

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -5,9 +5,11 @@ import jubilant
 
 logger = logging.getLogger(__name__)
 
+APP_NAME = "conserver"
+
 
 def test_deploy(charm: Path, juju: jubilant.Juju, config_file: str):
     """Deploy the charm under test."""
     config = {"config-file": config_file}
-    juju.deploy(charm.resolve(), config=config, app="conserver")
+    juju.deploy(charm.resolve(), config=config, app=APP_NAME)
     juju.wait(jubilant.all_active, timeout=300)
