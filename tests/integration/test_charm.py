fbee9c5cd95ed623c0e02b8158738dc8032a6feb
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-01-16 13:43:27 -0500

    chore: add workload version integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -13,3 +13,11 @@ def test_deploy(charm: Path, juju: jubilant.Juju, config_file: str):
     config = {"config-file": config_file}
     juju.deploy(charm.resolve(), config=config, app=APP_NAME)
     juju.wait(jubilant.all_active, timeout=300)
+
+
+def test_workload_version_is_set(juju: jubilant.Juju):
+    """Test that the workload version is set after deployment."""
+    version = juju.status().apps[APP_NAME].version
+    task = juju.exec("conserver -V", unit=f"{APP_NAME}/0")
+    version_output = task.stdout.strip()
+    assert f"conserver.com version {version}" in version_output
